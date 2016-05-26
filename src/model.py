506fa740851328537473480f090f5cbdfcec4c1f
Jim Mahoney <jim@mahoney.cc>
2016-05-26 17:25:15 -0400

    peewee model.py passes initial tests

--- src/model.py
+++ src/model.py
@@ -1,15 +1,50 @@
 # -- coding: utf-8 --
 """
- model2.py
+ model.py
  
- data classes for the umber app, built on the pewee ORM with a sqlite3 database
+ data classes for the umber app
+ built on the pewee ORM with a sqlite3 database
 
- >>> 
-  
+ Examples :
+
+   >>> populate_database()
+   Populating database with default data.
+ 
+   # Find the people and their role in a course given its name.
+   >>> demo = Course.get(Course.name == 'Demo Course')
+   >>> for (person, role) in sorted(demo.person_to_role.items(),
+   ...                              key = lambda x: x[0].name):
+   ...   print "{} is {} in {}.".format(person.name, role.name, demo.name)
+   ...
+   Jane Q. Doe is student in Demo Course.
+   Johnny Smith is student in Demo Course.
+   Ted Teacher is faculty in Demo Course.
+ 
+   # Find a person from their username.
+   >>> john = Person.get(username='johnsmith')
+   >>> print john.name
+   Johnny Smith
+
+   # Change their name.
+   >>> john.name = 'John Z. Smith'
+   >>> rows_changed = john.save()
+
+   # See the change.
+   >>> newjohn = Person.get(username='johnsmith')
+   >>> print newjohn.name
+   John Z. Smith
+
+   # Change it back.
+   >>> john.name = 'Johnny Smith'
+   >>> rows_changed = john.save()
+
+
 """
 
 from settings import db_path
-from peewee import SqliteDatabase
+from werkzeug.security import generate_password_hash, check_password_hash
+from peewee import SqliteDatabase, Model, BaseModel, \
+     TextField, IntegerField, PrimaryKeyField, ForeignKeyField
 
 db = SqliteDatabase(db_path)
 
@@ -18,7 +53,14 @@ class UnknownField(object):
 
 class BaseModel(Model):
     class Meta:
-        database = db
+        database = db   # peewee requires the name 'database' here.
+
+    def __repr__(self):
+        # e.g. 
+        fields = ', '.join(map(lambda x: '{}={}'.format(x[0],x[1]),
+                           self.__dict__['_data'].items()))
+        return '<{}({}) at 0x{:X}>'.format(self.__class__.__name__,
+                                       fields, id(self))
         
     @classmethod
     def first(cls):
@@ -28,27 +70,34 @@ class BaseModel(Model):
     def all(cls):
         return list(cls.select().execute())
 
+    @classmethod
+    def get_by_name(cls, name):
+       return cls.get(cls.name == name)
+    
 class Person(BaseModel):
     class Meta:
         db_table = 'Person'
 
+    person_id = PrimaryKeyField(db_column='person_id')
+        
     crypto = TextField()
     email = TextField()                
     ldap = IntegerField(db_column='ldap_id', index=True)
     name = TextField()
     notes = TextField()
     password = TextField()
-    person = PrimaryKeyField(db_column='person_id')
     username = TextField(unique=True)
-    
-    # -- Jim's extensions --
-    
+
+    def set_password(self, passwordtext):
+        self.password = generate_password_hash(passwordtext)
+        
     def works(self, course):
-        query = (Work.select().
+        query = (Work.select()
                      .where( Work.person == self,
                              Work.course == course ))
         return list(query.execute())
-        
+
+            
 class Course(BaseModel):
     class Meta:
         db_table = 'Course'
@@ -64,13 +113,25 @@ class Course(BaseModel):
     notes = TextField()
     path = TextField(unique=True)
     start_date = TextField()
-
+    
     def prepared(self):
-        query = (Person.select()
-                       .join(Registration)
-                       .where(Registration.course == self))
-        self.persons = list(query.execute())
-            
+        self.person_to_role = {reg.person : reg.role
+            for reg in (Registration.select(Registration.person,
+                                            Registration.role)
+                                    .where(Registration.course == self))}
+        #persons_query = (Person.select()
+        #                       .join(Registration)
+        #                       .where(Registration.course == self))
+        #self.persons = list(persons_query.execute())
+        #self.username_to_person = {p.username : p for p in self.persons}
+        #self.rolename_to_persons = \
+        #  {role.name : list(Person.select()
+        #                          .join(Registration)
+        #                          .where((Registration.course == self) &
+        #                                 (Registration.role == role))
+        #                          .execute()) \
+        #   for role in Role.all()}
+        
 class Assignment(BaseModel):
     class Meta:
         db_table = 'Assignment'
@@ -97,6 +158,34 @@ class Role(BaseModel):
     
     name = TextField()
     rank = IntegerField()
+
+    name_rank = {'admin': 5,
+                 'faculty': 4,
+                 'student': 3,
+                 'guest': 2,
+                 'visitor': 1
+                 }
+    name_alias = {'admin':           'admin',
+                  'administrator':   'admin',
+                  'faculty':         'faculty',
+                  'student':         'student',
+                  'students':        'student',
+                  'class':           'student',
+                  'guest':           'guest',
+                  'all':             'visitor',
+                  'any':             'visitor',
+                  'visitor':         'visitor'
+                  }
+    @staticmethod
+    def unalias(alias):
+        """ Convert alias to its standard role name. """
+        return Role.name_alias[alias]
+
+    @staticmethod
+    def create_defaults():
+        with db.transaction():
+            for (name, rank) in Role.name_rank.items():
+                Role.get_or_create(name=name, rank=rank)
 
 class Registration(BaseModel):
     class Meta:
@@ -141,12 +230,113 @@ class Work(BaseModel):
                              db_column='person_id',
                              to_field='person_id')
 
+
+def populate_database():
+    """ Create (and commit) the default database objects """
+    #
+    # Put some initial data into the database tables:
+    # Roles, Course 'demo' and its example Persons,
+    # Registrations, Assignments, and Works.
+    #
+    # The Roles data must be in place for the login system to work.
+    # The rest of this is just for examples and tests.
+    #
+    # The sqlite database must already exist before this is run.
+    # Create it with ../database/init_db.
+    #
+    # populate_db() is mostly idempotent; that is, runing multiple times
+    # is no different than running it once. The one thing that will
+    # change is the random seeds for the demo course sample users.
+
+    print "Populating database with default data."
     
+    Role.create_defaults()
+    
+    with db.transaction():
+        
+        student = Role.get(name = 'student')
+        faculty = Role.get(name = 'faculty')
+        
+        (democourse, created) = Course.get_or_create(
+                name = 'Demo Course',
+                name_as_title = 'Demo<br>Course',
+                path = 'demo',
+                start_date = '2013-01-01')
+        
+        (jane, created) = Person.get_or_create(
+            username = 'janedoe',
+            name = 'Jane Q. Doe',
+            email = 'janedoe@fake.address')
+        
+        (john, created) = Person.get_or_create(
+            username = 'johnsmith',
+            name = 'Johnny Smith',
+            email = 'johnsmith@fake.address')
+        
+        (ted, created)  = Person.get_or_create(
+            username = 'tedteacher',
+            name = 'Ted Teacher',
+            email = 'ted@fake.address')
+        
+        # The hashed seeded passwords are different each time,
+        # so they shouldn't be put in args to find_or_create. Otherwise, the
+        # new passwords won't be found and duplicate people would be created.
+        jane.set_password('test')
+        john.set_password('test')
+        ted.set_password('test')
+        
+        Registration.get_or_create(
+            person = john,
+            course = democourse,
+            role = student,
+            date = '2013-01-02')
+        
+        Registration.get_or_create(
+            person = jane,
+            course = democourse,
+            role = student,
+            date = '2013-01-03')
+        
+        Registration.get_or_create(
+            person = ted,
+            course = democourse,
+            role = faculty,
+            date = '2013-01-04')
+        
+        (assign1, created) = Assignment.get_or_create(
+            course = democourse,
+            nth = 1,
+            name = 'week 1',
+            uriname = 'week_1',
+            due = '2013-01-20',
+            blurb = 'Do chap 1 exercises 1 to 10.')
+        
+        (assign2, created) = Assignment.get_or_create(
+            course_id = democourse,
+            nth = 1,
+            name = 'week 2',
+            uriname = 'week_2',
+            due = '2013-01-27',
+            blurb = 'Write a four part fugue.')
+        
+        Work.get_or_create(
+            person = john,
+            assignment = assign1,
+            submitted = '2013-01-20 18:19:20',
+            grade = 'B')
+        
+        Work.get_or_create(
+            person = jane,
+            assignment = assign1,
+            submitted = '2013-01-21 16:01:01',
+            grade = 'B-')
+
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
 
-    
+
 # ------------------------------------------------------
 # peewee database notes
 #
@@ -160,7 +350,7 @@ if __name__ == '__main__':
 # * foreign keys and relationships
 #     the correspondance between database column names and class fields
 #     is set explicitly in the pewee class definition.
-      See the docs for ForeignKeyField
+#     See the docs for ForeignKeyField
 #
 # * performance & foreign keys
 #     See http://docs.peewee-orm.com/en/latest/peewee/querying.html#nplusone
@@ -206,3 +396,25 @@ if __name__ == '__main__':
 #    is called after that data is loaded ... which is
 #    the right place to put what would otherwise go in __init__.
 #
+# * transactions
+#    See http://docs.peewee-orm.com/en/latest/peewee/transactions.html :
+#    peewee has some syntax for marking blocks as atomic
+#    However, peewee's sqlite egine, based on pysql, is autocommit
+#    by default and somewhat awkward to use otherwise.
+#    (Turning off autocommit requires explict "begin" or "transaction".)
+#    The suggested syntax is either
+#      with db.transaction():
+#        do_stuff()
+#    or
+#      @db.transaction()
+#      def do():
+#        stuff()
+#    I think the "with" syntax is clearer - see
+#    http://effbot.org/zone/python-with-statement.htm
+#    for a discussion of what it is for in python -
+#    and doesn't require a function definition.
+#
+# * get
+#   peewee spells "find" as "get".
+#   In other ORMs, get seems to often be only "get by id",
+#   whereas find() or fetch() is by select() specifications.
