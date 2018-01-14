24b5c6dda90253c96c5b8844326ebb3c51311ff4
Jim Mahoney <jim@mahoney.cc>
2018-01-14 11:53:32 -0500

    cleaning up before deploy

--- src/settings.py
+++ src/settings.py
@@ -1,102 +1,143 @@
 """
  settings.py
 
- Project site-specific constants (file and url paths, ports, etc)
+ (1) Set an environment variable before running this :
 
-    secret_key
-    os_root         # e.g. /Users/mahoney/sugar/academics/umber
-    os_basename     # e.g. courses , folder with content pages
-    os_base         # e.g. /Users/mahoney/sugar/academics/umber/courses
-    os_src          # e.g. /Users/mahoney/sugar/academics/umber/src
-    url_basename    # e.g. umber , url prefix http://host/umber/...
-    http_port
-    https_port
-    timezone
-    db_path
+       $ export UMBER_CONFIG = DEVELOPMENT | PRODUCTION
 
- URLs have the form
-     host url_basename path
- e.g.
-     http://localhost:8080/  umber         /  demo       / home
-            host             url_basename    course_path  page_path
+ (2) Edit the variables marked with "customize" to match your installation.
 
- os paths to files files are
-     os_base path
- e.g.
-     /Users/mahoney/academics/umber/courses  / demo        /   home
-     os_base                                   course_path     page_path
-
- where (os_base = os_root os_basename)
-
- See the Page class in model.py for more details.
-
- ------------
-
- Part of this file is also acting as flask's configuration file;
- its path is fed to the flask app. See the config.py notes below.
+ ------------------------------------------------------
  
+ The umber urls have this form :
+                             
+        PROTOCOL  SERVER_NAME    URL_BASE    page_path
+   e.g. https://  umber.cc:433 / umber    /  demo/home
+
+ Its named folders and file constants are
+
+        OS_COURSES  e.g. OS_ROOT/courses   
+        OS_DB       e.g. OS_ROOT/database/umber.sql
+        OS_GIT      e.g. OS_ROOT
+
+ as well as
+
+        OS_ROOT    e.g. /Users/mahoney/sugar/academics/umber
+        which is computed from the location of this file.
+
+ -------------------------------------------------------------------
+
+ See the Page class in model.py for more details for urls and paths.
+
+ Note that this file is loaded from umber.py with app.config_from_file().
+ (And its defined constants are ALL_CAPS to be consistent with Flask.)
+
 """
-import sys, os
+import os, sys, datetime
+from os.path import abspath, join, realpath
 
-url_basename = 'umber'    # URL prefix for umber courses
-_os_basename = 'cours'    # course folder within project folder os_root
+OS_ROOT = abspath(join(realpath(__file__), '..'))
 
-git_base = 'courses'     # root of git repo; append page.path for its git path
+if os.environ('UMBER_CONFIG') == 'DEVELOPMENT':
 
-host = '127.0.0.1:5000'  # host:port      : for constructing breadcrumb url
-protocol = 'http'        # http | https
+    PROTOCOL = 'http://'
+    SERVER_NAME = '127.0.0.1:5000'
+    URL_BASE = 'umber'
+    ADMIN_CONTACT_URL = '<a mailto:"adam@fake.fake">Adam Administrator</a>'  # ** Customize **
+    
+    OS_COURSES = os.path.join(OS_ROOT, 'courses')
+    OS_DB = os.path.join(OS_ROOT, 'database', 'umber.sql')
+    OS_GIT = OS_ROOT
 
-umber_url_base = protocol + '://' + host + '/' + url_basename
+    DEBUG = True
+    TESTING = True
+    SECRET_KEY = 'seekrit'
+    SESSION_COOKIE_NAME = 'umber'
+    PERMANENT_SESSION_LIFETIME = datetime.timedelta(days=1)
+    LOCALTIMEZONE = 'US/Eastern'
+    LOCALTIMEZONEOFFSET = '-05:00'
 
-admin_email = '<a mailto:"mahoney@marlboro.edu">Jim Mahoney</a>' 
-about_copyright_url = umber_url_base + '/site/docs/about'
-help_url = umber_url_base + '/site/docs/help'
+elif os.environ('UMBER_CONFIG') == 'PRODUCTION':
+                                                                       #
+    PROTOCOL = 'http://'                                               # 
+    SERVER_NAME = 'cs.marlboro.college'                                #  c
+    URL_BASE = 'cours'                                                 #  u
+    ADMIN_CONTACT_URL = '<a mailto:"jim@mahoney.cc">Jim Mahoney</a>'   #  s
+                                                                       #  t
+    OS_COURSES = '/var/www/cours'                                      #  m
+    OS_DB = '/var/www/cours/_umber_marlboro.sql'                       #  i
+    OS_GIT = '/var/www/cours'                                          #  z
+                                                                       #  e
+    DEBUG = False                                                      #
+    TESTING = False                                                    #
+    SECRET_KEY = 'seeekrit_marlboro'                                   #
+    SESSION_COOKIE_NAME = 'umber_marlboro'                             #
+    PERMANENT_SESSION_LIFETIME = datetime.timedelta(days=1)            #
+    LOCALTIMEZONE = 'US/Eastern'                                       #
+    LOCALTIMEZONEOFFSET = '-05:00'                                     #
+    
+else:
+    raise Exception('Oops: UMBER_CONFIG environment variable is undefined.')
 
-localtimezone = 'US/Eastern'
-localtimezoneoffset = '-05:00'  # for ISO GMT strings
+UMBER_URL = PROTOCOL + SERVER_NAME + '/' + URL_BASE
+ABOUT_URL = UMBER_URL + '/site/docs/about'
+HELP_URL = UMBER_URL + '/site/docs/help'
+PHOTOS_URL = UMBER_URL + '/site/photos'
 
-def _get_os_root():
-    """ Return absolute path to project folder, without trailing slash."""
-    # i.e. the folder above src/ which is what this settings.py file is in.
-    # On my mac develepment laptop this is /Users/mahoney/academics/umber
-    # (This is also UMBER_ROOT in the project environment.)
-    my_folder = os.path.dirname(os.path.realpath(__file__))
-    parent_folder = os.path.abspath(my_folder + '/..')
-    return parent_folder
+OS_SRC = join(OS_ROOT, 'src')
+OS_TEMPLATE = join(OS_ROOT, 'templates')
+OS_STATIC = join(OS_ROOT, 'static')
 
-os_root = _get_os_root()
-os_src = os.path.join(os_root, 'src')
-os_template = os.path.join(os_root, 'templates')
-os_static = os.path.join(os_root, 'static')
-
-# ***
-# root of folder with course files
-os_base = os.path.join(os_root, _os_basename) 
-
-# user images , <username>.jpg ,
-# about (122 x 152) px (what nook.marlboro.edu is using Jan 2018)
-photo_folder_url = umber_url_base + '/site/photos/'
-
-db_path = os_root + '/database/umber.db'   # absolute sqlite3 file path
-
-os_config = os.path.join(os_src, 'settings.py')  # ... this file (!)
-
-# --------------
-# os_config = os.path.join(os_src, 'config.py')
-# -------------------
-# What's below here was in config.py ,
-# and may need multiple versions for different deployment situations.
+# =============================================================================
 #
-# This part is the flask configuration file ;
-# see http://flask.pocoo.org/docs/0.12/config/
-
-import datetime
-
-TESTING = True                 # TEST & DEBUG 
-DEBUG = True                   # 
-
-SECRET_KEY = 'umber seekrit'   # TEST & DEBUG 
-
-SESSION_COOKIE_NAME = 'umber_session'
-PERMANENT_SESSION_LIFETIME = datetime.timedelta(days=1)
+# url_basename = 'umber'    # URL prefix for umber courses
+# _os_basename = 'cours'    # course folder within project folder os_root
+#
+# git_base = 'courses'     # root of git repo; append page.path for its git path
+#
+# host = '127.0.0.1:5000'  # host:port      : for constructing breadcrumb url
+# protocol = 'http'        # http | https
+#
+# umber_url_base = protocol + '://' + host + '/' + url_basename
+#
+# about_copyright_url = umber_url_base + '/site/docs/about'
+# help_url = umber_url_base + '/site/docs/help'
+#
+# # ----------------------------------------------------------
+#
+# os_root = _get_os_root()
+# os_src = os.path.join(os_root, 'src')
+# os_template = os.path.join(os_root, 'templates')
+# os_static = os.path.join(os_root, 'static')
+#
+# # ***
+# # root of folder with course files
+# os_base = os.path.join(os_root, _os_basename) 
+#
+# # user images , <username>.jpg ,
+# # about (122 x 152) px (what nook.marlboro.edu is using Jan 2018)
+# photo_folder_url = umber_url_base + '/site/photos/'
+#
+# db_path = os_root + '/database/umber.db'   # absolute sqlite3 file path
+#
+# os_config = os.path.join(os_src, 'settings.py')  # ... this file (!)
+#
+# # --------------
+# # os_config = os.path.join(os_src, 'config.py')
+# # -------------------
+# # What's below here was in config.py ,
+# # and may need multiple versions for different deployment situations.
+# #
+# # This part is the flask configuration file ;
+# # see http://flask.pocoo.org/docs/0.12/config/
+#
+# import datetime
+#
+# TESTING = True                 # TEST & DEBUG 
+# DEBUG = True                   # 
+#
+# SECRET_KEY = 'umber seekrit'   # TEST & DEBUG 
+#
+# SESSION_COOKIE_NAME = 'umber_session'
+# PERMANENT_SESSION_LIFETIME = datetime.timedelta(days=1)
 
