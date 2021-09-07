44609909c8ba5dcfb66afc7b01d043228478cb7c
Jim Mahoney <jim@mahoney.cc>
2021-09-07 18:34:39 +0000

    added .swift mime type

--- src/settings.py
+++ src/settings.py
@@ -112,7 +112,7 @@ photos_url = umber_url + '/umber/photos'
 umber_mime_types = {x:'text/plain' for x in
     ('.py', '.c', '.cc', '.cpp', '.c++', '.c#', '.dot', '.lisp',
      '.java', '.hs', '.clisp', '.pl', '.tex', '.rb', '.yvtm', '.vtm', '.csv',
-     '.i', '.s', '.objdump', '.sql', '.scm', '.rkt', '.rs', '.out'
+     '.i', '.s', '.objdump', '.sql', '.scm', '.rkt', '.rs', '.out', '.swift'
     )}
 # image mime type variations
 umber_mime_types['.PNG'] = 'image/png'
